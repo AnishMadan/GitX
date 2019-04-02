0c2f7d20fcb7972e84779ecfd3f385879532628c
Anish Madan <anishmadan10@gmail.com>
2019-04-01 20:40:08 -0400

    repo path logic

--- libgitX.py
+++ libgitX.py
@@ -55,5 +55,23 @@ class GitRepository(object):
                 raise Exception("Unsupported repositoryformatversion %s" % vers)
 
 def repo_path(repo, *path):
-    """Compute path under repo's gitdir."""
     return os.path.join(repo.gitdir, *path)
+
+def repo_file(repo, *path, mkdir=False):
+    if repo_dir(repo, *path[:-1], mkdir=mkdir):
+        return repo_path(repo, *path)
+
+def repo_dir(repo, *path, mkdir=False):
+    path = repo_path(repo, *path)
+
+    if os.path.exists(path):
+        if (os.path.isdir(path)):
+            return path
+        else:
+            raise Exception("Not a directory %s" % path)
+
+    if mkdir:
+        os.makedirs(path)
+        return path
+    else:
+        return None
